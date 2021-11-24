1830f8018d573c7e74aeee0666a339be0a4f5dfe
Steve <stevebarakat@gmail.com>
2021-11-24 15:17:40 -0500

    pwa2

--- hooks/useOnScreen.js
+++ hooks/useOnScreen.js
@@ -1,9 +1,9 @@
-import { useState, useLayoutEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function useOnScreen(ref, rootMargin = "0px") {
   // State and setter for storing whether element is visible
   const [isIntersecting, setIntersecting] = useState(false);
-  useLayoutEffect(() => {
+  useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
         // Update our state when observer callback fires
