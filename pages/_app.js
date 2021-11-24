e8c45f4ecb0da72dda42415a19e103eb7aa1ce45
Steve <stevebarakat@gmail.com>
2021-11-24 15:21:06 -0500

    pwa3

--- pages/_app.js
+++ pages/_app.js
@@ -10,13 +10,6 @@ import "../styles/variables.css";
 import "../styles/globals.css";
 import "../styles/plans.css";
 
-// register service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", async () => {
-    await navigator.serviceWorker.register("./sw.js");
-  });
-}
-
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
