879f59d80496d09f529c02ee0afba22343b36abc
Steve <stevebarakat@gmail.com>
2021-11-27 10:08:02 -0500

    removed lazy loading footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import loadable from "@loadable/component";
 import Header from "../components/Header";
-// import Footer from "../components/Footer";
-const Footer = loadable(() => import("./Footer"));
+import Footer from "../components/Footer";
+// const Footer = loadable(() => import("./Footer"));
 
 const Layout = ({ children }) => (
   <>
