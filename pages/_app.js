009cd76bae0cb3144f0f835a6ed59c69057a2d79
Steve <stevebarakat@gmail.com>
2021-11-28 13:27:51 -0500

    removed meta from app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../lib/apollo";
-import Head from "next/head";
 import "@fontsource/montserrat/400.css";
 import "@fontsource/montserrat/800.css";
 import "@fontsource/inter/variable.css";
@@ -12,12 +11,6 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
-      </Head>
       <ApolloProvider client={client}>
         <Component {...pageProps} />
       </ApolloProvider>
